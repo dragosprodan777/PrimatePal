0e3d7eb40c5b895230d96bba471d217ed180eb99
dragosprodan777 <dragosprodan777@icloud.com>
2023-07-13 19:28:59 +0200

    0.1.3: bot is connected to server

--- PrimatePal-code/main.py
+++ PrimatePal-code/main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+from discord.ext import commands
+import discord
+
+BOT_TOKEN = "MTEyODgxODY3MzEyNjIyMzk2Mw.Gfg8DN.fEp3U3JZ_EOY54v0SxtmS89ZTxdjkKPyBnq1fY"
+MIERCURI_ID = 334750929637343232
+BOT_COMMANDS_ID = 787686148889509918
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("I am ready!")
+
+bot.run(BOT_TOKEN)
