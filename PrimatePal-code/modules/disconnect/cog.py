19984ac68df4d294a26d93bff5c164c61f38333a
dragosprodan777 <dragosprodan777@icloud.com>
2023-09-20 02:19:06 +0200

    0.2.3.2 FIX: added the correct files to commit

---
+++ PrimatePal-code/modules/disconnect/cog.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+
+
+class Cog(commands.Cog, name="Leave"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def leave(self, ctx):
+        print("Received 'leave' command in disconnect/cog.py")
+
+        # Check if the bot is connected to a voice channel in the same guild as the command
+        if ctx.voice_client:
+            # Disconnect from the voice channel
+            await ctx.voice_client.disconnect()
+            await ctx.send("I have left the voice channel.")
+        else:
+            await ctx.send("I'm not in a voice channel.")
+
+
+async def setup(bot):
+    await bot.add_cog(Cog(bot))
